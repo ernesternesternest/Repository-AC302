6f8a723952f9d96af85d03b67d55d3fcc1503ff9
ernesternesternest <ernestpresh@gmail.com>
2021-02-06 14:27:30 +0800

    project 5

--- lesson5/app.js
+++ lesson5/app.js
@@ -10,21 +10,35 @@ var HEIGHT = 400;
 var x,y;
 var mx, my; 
 
+//challenge color
+var circleColor = "rgb(255,0,0)";
+
+//challenge ball 2
+var x2,y2;
+var mx2, my2; 
+var circleColor2 = "rgb(0,255,0)";
+
 //initialize animation
 function init(){
 	x = 300;
 	y = 200;
 	mx = 3;
 	my = 4;
+	//challenge ball 2
+	x2 = 100;
+	y2 = 300;
+	mx2 = 4;
+	my2 = 3;
+
 	return setInterval(draw,10);
 }
 //draw circle
-function circle(x,y,r){
+function circle(x,y,r,color){
 	ctx.beginPath();
 	ctx.arc(x,y,r,0, 6.28);
 	ctx.closePath();
 	ctx.stroke();
-	ctx.fillStyle = "red";
+	ctx.fillStyle = color;
 	ctx.fill();
 }
 //clear trail
@@ -32,20 +46,46 @@ function clear(){
 	ctx.clearRect(0,0,WIDTH,HEIGHT);
 }
 
+//challenge random color
+function randomColor(){
+	var r = Math.floor(Math.random()*255);
+	var g = Math.floor(Math.random()*255);
+	var b = Math.floor(Math.random()*255);
+	return "rgb(" + r + "," + g + "," + b +")";
+}
+
+
 //draw frames
 function draw(){
 	clear();
-	circle(x,y,30);
+	circle(x,y,30,circleColor);
 
 	if(x+mx <0 || x+mx>WIDTH){
 		mx = -mx;
+		circleColor = randomColor();
 	}
 	if(y+my <0 || y+my>HEIGHT){
 		my = -my;
+		circleColor = randomColor();
 	}
 
 	x += mx;
 	y += my;
+
+	//challenge ball 2
+	circle(x2,y2,30,circleColor2);
+
+	if(x2+mx2 <0 || x2+mx2>WIDTH){
+		mx2 = -mx2;
+		circleColor2 = randomColor();
+	}
+	if(y2+my2 <0 || y2+my2>HEIGHT){
+		my2 = -my2;
+		circleColor2 = randomColor();
+	}
+
+	x2 += mx2;
+	y2 += my2;
 }
 
 init();
